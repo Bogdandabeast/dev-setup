f107c130ee8cb0b39a150bd73392e192f0d47818
Bogdandabeast <bogdan.console@gmail.com>
2025-03-27 17:09:45 +0100

    syntax fix

--- files/scripts/cachykernel.sh
+++ files/scripts/cachykernel.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Verifica que el script se esté ejecutando como root
+set -euo pipefail
+
+
 if [ "$EUID" -ne 0 ]; then
   echo "Por favor, ejecuta este script como root o usando sudo."
   exit
 fi
 
-# Cambiar al directorio de repositorios
+
 cd /etc/yum.repos.d/ || { echo "Error al cambiar al directorio /etc/yum.repos.d/"; exit 1; }
 
-# Descargar el archivo del repositorio
+
 echo "Descargando el repositorio de kernel CachyOS..."
 wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
 
@@ -18,7 +20,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Realizar cambios en los paquetes del sistema con rpm-ostree
 echo "Modificando paquetes del sistema..."
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
 
