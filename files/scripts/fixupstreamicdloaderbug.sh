d43929024e28d55cae82fe72d787e7ece6e9c008
Bogdandabeast <bogdan.console@gmail.com>
2025-11-04 21:50:00 +0100

    from-43-to-42

--- files/scripts/fixupstreamicdloaderbug.sh
+++ files/scripts/fixupstreamicdloaderbug.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332439
+# mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
 # swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
 rpm-ostree override replace \
   --from repo='fedora' \
