1b2f37ebfb2e941460345a74f8e35396a7e80de2
Bogdandabeast <bogdan.console@gmail.com>
2025-12-17 00:16:43 +0100

    feat: Test winboat

--- files/scripts/addbrewjustimport.sh
+++ files/scripts/addbrewjustimport.sh
@@ -2,6 +2,17 @@
 
 # Tell build process to exit if there are any errors.
 set -oue pipefail
+
+# Descargar el archivo .rpm del último release de winboat
+RPM_URL=$(curl -s https://api.github.com/repos/TibixDev/winboat/releases/latest \
+  | grep "browser_download_url" \
+  | grep ".rpm" \
+  | cut -d '"' -f 4)
+
+echo "Descargando RPM desde: $RPM_URL"
+curl -L -o /tmp/winboat.rpm "$RPM_URL"
+
+# Añadir import al justfile
 echo '
 
 import "/usr/share/ublue-os/just/50-brew.just"
